b77a37b63de0f5acb7f7d386c8319119925169d5
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-02-18 13:00:34 +0200

    Adds turn off all appliances service

--- src/components/Appliances/index.js
+++ src/components/Appliances/index.js
@@ -7,23 +7,66 @@ import "./Appliances.css";
 const Appliances = () => {
   const { hass } = useHass();
   const { states } = hass;
-  const entityIds = [
-    "fan.air_purifier",
-    "switch.ps5_power",
-    "switch.kitchen_toaster_plug",
-    "vacuum.roborock_s6",
-    "humidifier.deerma_jsq2w_dbfc_humidifier",
+  const devices = [
+    {
+      entity_id: "fan.air_purifier",
+      turn_off_service: {
+        domain: "fan",
+        action: "turn_off",
+      },
+    },
+    {
+      entity_id: "switch.ps5_power",
+      turn_off_service: {
+        domain: "switch",
+        action: "turn_off",
+      },
+    },
+    {
+      entity_id: "switch.kitchen_toaster_plug",
+      turn_off_service: {
+        domain: "switch",
+        action: "turn_off",
+      },
+    },
+    {
+      entity_id: "vacuum.roborock_s6",
+      turn_off_service: {
+        domain: "vacuum",
+        action: "turn_off",
+      },
+    },
+    {
+      entity_id: "humidifier.deerma_jsq2w_dbfc_humidifier",
+      turn_off_service: {
+        domain: "humidifier",
+        action: "turn_off",
+      },
+    },
   ];
 
-  const entities = entityIds.map((entityId) => states[entityId]);
+  const entities = devices.map(({ entity_id, turn_off_service }) => ({
+    ...states[entity_id], entity_id, turn_off_service
+  }));
 
   const entitiesOn = entities.filter((entity) => entity.state === "on");
+
+  const turnAllDevicesOff = () => {
+    entitiesOn.map(({ entity_id, turn_off_service }) =>
+      hass.callService(turn_off_service.domain, turn_off_service.action, {
+        entity_id,
+      })
+    );
+  };
 
   return (
     <div className="">
       <FontAwesomeIcon icon={faPlug} size="3x" />
       <p>Appliances</p>
       <p>{entitiesOn.length ? `${entitiesOn.length} on` : `Clear`}</p>
+      {!!entitiesOn.length && (
+        <button onClick={() => turnAllDevicesOff()}>Turn all off</button>
+      )}
     </div>
   );
 };
