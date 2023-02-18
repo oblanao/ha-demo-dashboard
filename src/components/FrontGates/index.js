713cc580c3a59f41fe6a5706588e931da3511f85
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-02-18 12:21:05 +0200

    Adds open and close gates button

--- src/components/FrontGates/index.js
+++ src/components/FrontGates/index.js
@@ -9,11 +9,20 @@ const FrontGates = () => {
   const { states } = hass;
   const data = states["binary_sensor.front_gates_sensor_contact"];
 
+  const toggleGates = () =>
+    hass.callService("switch", "turn_on", {
+      entity_id: "switch.front_gates",
+    });
+
   return (
     <div className="front-gates">
       <FontAwesomeIcon icon={faDoorClosed} size="3x" />
       <p>Front Gates</p>
       <p>{data.state === "off" ? "Closed" : "Open"}</p>
+
+      <button onClick={toggleGates}>
+        {data.state === "off" ? "Open" : "Close"} Gates
+      </button>
     </div>
   );
 };
