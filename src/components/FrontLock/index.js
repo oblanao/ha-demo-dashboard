2b266d956d6068b1a162fcc14abb4b2b15d93615
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-02-18 12:25:09 +0200

    Adds lock/open services for front lock

--- src/components/FrontLock/index.js
+++ src/components/FrontLock/index.js
@@ -9,11 +9,27 @@ const FrontLock = () => {
   const { states } = hass;
   const data = states["lock.front_lock_lock"];
 
+  const lockDoor = () =>
+    hass.callService("lock", "lock", {
+      entity_id: "lock.front_lock_lock",
+    });
+
+  const openDoor = () =>
+    hass.callService("lock", "open", {
+      entity_id: "lock.front_lock_lock",
+    });
+
   return (
     <div className="front-lock">
       <FontAwesomeIcon icon={faLock} size="3x" />
       <p>Front Lock</p>
       <p>{data.state}</p>
+      {data.state === "unlocked" && (
+        <button onClick={() => lockDoor()}>Lock</button>
+      )}
+      {data.state === "locked" && (
+        <button onClick={() => openDoor()}>Unlock</button>
+      )}
     </div>
   );
 };
