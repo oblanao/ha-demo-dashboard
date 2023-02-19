207a6550647f97e16e9c86093e1750dd897c1487
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-02-19 13:56:45 +0200

    Adds icons and styles to front lock dashboard card

--- src/components/FrontLock/index.js
+++ src/components/FrontLock/index.js
@@ -1,36 +1,44 @@
-import { faLock } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faLock } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useHass } from "../../hooks";
+import { DashboardCard } from "../ui-elements";
 import "./FrontLock.css";
+import lockClosed from "../../resources/icons/lock_closed.png";
+import lockOpen from "../../resources/icons/lock_open.png";
 
 const FrontLock = () => {
   const { hass } = useHass();
   const { states } = hass;
   const data = states["lock.front_lock_lock"];
 
-  const lockDoor = () =>
-    hass.callService("lock", "lock", {
-      entity_id: "lock.front_lock_lock",
-    });
+  // const lockDoor = () =>
+  //   hass.callService("lock", "lock", {
+  //     entity_id: "lock.front_lock_lock",
+  //   });
 
-  const openDoor = () =>
-    hass.callService("lock", "open", {
-      entity_id: "lock.front_lock_lock",
-    });
+  // const openDoor = () =>
+  //   hass.callService("lock", "open", {
+  //     entity_id: "lock.front_lock_lock",
+  //   });
+
+  const isUnlocked = data.state === "unlocked";
 
   return (
-    <div className="front-lock">
-      <FontAwesomeIcon icon={faLock} size="3x" />
-      <p>Front Lock</p>
-      <p>{data.state}</p>
-      {data.state === "unlocked" && (
-        <button onClick={() => lockDoor()}>Lock</button>
-      )}
-      {data.state === "locked" && (
-        <button onClick={() => openDoor()}>Unlock</button>
-      )}
-    </div>
+    <DashboardCard>
+      <div className="front-lock">
+        <img src={isUnlocked ? lockOpen : lockClosed} />
+        {/* <FontAwesomeIcon icon={faLock} size="3x" /> */}
+        <h3>Front Lock</h3>
+        {/* <p>{data.state}</p>
+        {data.state === "unlocked" && (
+          <button onClick={() => lockDoor()}>Lock</button>
+        )}
+        {data.state === "locked" && (
+          <button onClick={() => openDoor()}>Unlock</button>
+        )} */}
+      </div>
+    </DashboardCard>
   );
 };
 
