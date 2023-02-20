87270ed0d4380cc493032a6926d199641e939d91
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-02-20 16:08:32 +0200

    adds styles and dates to hello message

--- src/components/HelloMessage/index.js
+++ src/components/HelloMessage/index.js
@@ -1,6 +1,9 @@
+import { format } from "date-fns";
 import React from "react";
 import { useHass } from "../../hooks";
+import { DashboardCard } from "../ui-elements";
 import "./HelloMessage.css";
+import smartHome from "../../resources/icons/smart-home.png";
 
 const HelloMessage = () => {
   const { hass } = useHass();
@@ -8,10 +11,17 @@ const HelloMessage = () => {
   const { name } = user;
 
   return (
-    <div className="hello-message">
-      <h3>Hello, {name}!</h3>
-      <h4></h4>
-    </div>
+    <DashboardCard>
+      <div className="hello-message">
+        <div className="icon flex-col-center">
+          <img src={smartHome} />
+        </div>
+        <div className="text">
+          <h3>Hello, {name}!</h3>
+          <h4>{format(new Date(), "eeee, MMMM dd")}</h4>
+        </div>
+      </div>
+    </DashboardCard>
   );
 };
 
