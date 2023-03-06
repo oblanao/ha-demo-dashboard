24082736ed0cbfc4d097a29ed101afb0a045128b
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-06 13:06:12 +0200

    removes commented code

--- src/components/FrontGates/index.js
+++ src/components/FrontGates/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { useHass } from "../../hooks";
-// import { faDoorClosed } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import gatesOpen from "../../resources/icons/gates_open.png";
 import gatesClosed from "../../resources/icons/gates_closed.png";
 import "./FrontGates.css";
-import { DashboardCard, WarningCard } from "../ui-elements";
+import { DashboardCard } from "../ui-elements";
 
 const FrontGates = () => {
   const { hass } = useHass();
@@ -23,13 +21,7 @@ const FrontGates = () => {
     <DashboardCard variant={!isClosed && "warning"}>
       <div className="front-gates flex-col-center">
         <img src={isClosed ? gatesClosed : gatesOpen} />
-        {/* <FontAwesomeIcon icon={faDoorClosed} size="3x" /> */}
         <h3>Front Gates</h3>
-        {/* <p>{isClosed ? "Closed" : "Open"}</p> */}
-
-        {/* <button onClick={toggleGates}>
-          {isClosed ? "Open" : "Close"} Gates
-        </button> */}
       </div>
     </DashboardCard>
   );
