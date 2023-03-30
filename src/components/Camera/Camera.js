72ad420146a19faefd60df008252be523748211b
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-30 11:46:19 +0300

    removes old action sheet from camera

--- src/components/Camera/Camera.js
+++ src/components/Camera/Camera.js
@@ -1,30 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useHass } from "../../hooks";
-import ActionSheet from "../ui-elements/ActionSheet/ActionSheet";
 import "./Camera.css";
 
 const Camera = ({ entity_id }) => {
   // @see: http://192.168.68.67:1984 (go2rtc)
-  const [isOpen, setOpen] = useState(false);
-  const handleClick = () => setOpen((open) => !open);
 
   const { hass } = useHass();
   const { states } = hass;
   const image = states[`camera.${entity_id}`].attributes.entity_picture;
 
-  return (
-    <>
-      <div onClick={handleClick}>
-        <img className="camera-image" src={image} />
-      </div>
-      <ActionSheet
-        isOpen={isOpen}
-        setOpen={setOpen}
-        subtitle={"My subtitle comes here"}
-        title={"My title"}
-      />
-    </>
-  );
+  return <img className="camera-image" src={image} />;
 };
 
 export default Camera;
