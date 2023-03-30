4eedae28f0278b9ddc3154153955115583ab04b6
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-30 11:35:08 +0300

    Adds action sheet to lights card

--- src/components/LightsOn/index.js
+++ src/components/LightsOn/index.js
@@ -3,6 +3,7 @@ import { useHass } from "../../hooks";
 import "./LightsOn.css";
 import { DashboardCard, UnavailableBadge } from "../ui-elements";
 import lightBulb from "../../resources/icons/lightbulb.png";
+import WithActionSheet from "../ui-elements/ActionSheet/WithActionSheet";
 
 const LightsOn = () => {
   const { hass } = useHass();
@@ -30,22 +31,39 @@ const LightsOn = () => {
   const entitiesUnavailable = entities.filter(
     (entity) => entity?.state === "unavailable"
   );
-  // const turnAllLightsOff = () =>
-  //   hass.callService("light", "turn_off", {
-  //     entity_id: "light.all_lights",
-  //   });
+
+  const turnAllLightsOff = () =>
+    hass.callService("light", "turn_off", {
+      entity_id: "light.all_lights",
+    });
 
   return (
-    <DashboardCard variant={entitiesOn?.length && "lights-on"}>
-      <div className="flex-col-center">
-        {!!entitiesUnavailable.length && (
-          <UnavailableBadge count={entitiesUnavailable.length} />
-        )}
-        <img src={lightBulb} />
-        <h3>Lights</h3>
-        <p>{!!entitiesOn.length ? `${entitiesOn.length} on` : `All off`}</p>
-      </div>
-    </DashboardCard>
+    <WithActionSheet
+      options={{
+        title: "Lights",
+        subtitle: entitiesOn.length
+          ? `There are ${entitiesOn.length} lights on.`
+          : `All lights are off`,
+        actions: [
+          entitiesOn.length && {
+            caption: "Lights off",
+            onClick: turnAllLightsOff,
+            isDestructive: false,
+          },
+        ],
+      }}
+    >
+      <DashboardCard variant={entitiesOn?.length && "lights-on"}>
+        <div className="flex-col-center">
+          {!!entitiesUnavailable.length && (
+            <UnavailableBadge count={entitiesUnavailable.length} />
+          )}
+          <img src={lightBulb} />
+          <h3>Lights</h3>
+          <p>{!!entitiesOn.length ? `${entitiesOn.length} on` : `All off`}</p>
+        </div>
+      </DashboardCard>
+    </WithActionSheet>
   );
 };
 
