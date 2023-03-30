a6aee97d487616057f2db5d9da9153c5483609d2
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-30 10:55:14 +0300

    Adds with action sheet HOC

--- src/components/ui-elements/ActionSheet/ActionSheet.js
+++ src/components/ui-elements/ActionSheet/ActionSheet.js
@@ -1,19 +1,20 @@
 import "./ActionSheet.css";
-import React from 'react';
+import React from "react";
 import ActionSheetContent from "./ActionSheetContent";
 
-const ActionSheet = ({
-  isOpen,
-  setOpen,
-  subtitle,
-  title = "ActionSheet Title",
-}) => {
+const ActionSheet = ({ isOpen, setOpen, subtitle, title, actions = [] }) => {
   return (
     <>
       {isOpen && (
         <div className="modal-backdrop" onClick={() => setOpen(false)}></div>
       )}
-      <ActionSheetContent show={isOpen} title={title} subtitle={subtitle} />
+      <ActionSheetContent
+        show={isOpen}
+        setShow={setOpen}
+        title={title}
+        subtitle={subtitle}
+        actions={actions}
+      />
     </>
   );
 };
--- src/components/ui-elements/ActionSheet/ActionSheetContent.js
+++ src/components/ui-elements/ActionSheet/ActionSheetContent.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 import withSlideAnimation from "./withAnimation";
 
-const ActionSheetContent = ({ show, title, subtitle }) => {
+const ActionSheetContent = ({ show, setShow, title, subtitle, actions }) => {
+  const handleCancel = () => {
+    setShow(false);
+  };
+
+  const showAction = ({ caption, onClick, isDestructive }) => (
+    <button
+      className={isDestructive ? "action-button destructive" : "action-button"}
+      onClick={onClick}
+      key={`btn-action-${caption}`}
+    >
+      {caption}
+    </button>
+  );
+
   return (
     <div className="actionsheet">
       <div className="actionsheet-contents">
@@ -10,12 +24,18 @@ const ActionSheetContent = ({ show, title, subtitle }) => {
           {!!subtitle && <p>{subtitle}</p>}
         </div>
         <div className="actionsheet-action-buttons">
-          <button className="action-button destructive">Option 1</button>
-          <button className="action-button">Option 2</button>
+          {actions.map(showAction)}
+          {/* <button className="action-button destructive">Option 1</button>
+          <button className="action-button">Option 2</button> */}
         </div>
       </div>
       <div className="actionsheet-contents">
-        <button className="action-button action-button-separate">Cancel</button>
+        <button
+          className="action-button action-button-separate"
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
       </div>
     </div>
   );
