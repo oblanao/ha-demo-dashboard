33c0c0f11f527c3e9f7a1da4629fab24c057b1e9
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-30 11:32:22 +0300

    Adds action sheet to front lock

--- src/components/FrontLock/index.js
+++ src/components/FrontLock/index.js
@@ -4,33 +4,48 @@ import { DashboardCard, UnavailableBadge } from "../ui-elements";
 import "./FrontLock.css";
 import lockClosed from "../../resources/icons/lock_closed.png";
 import lockOpen from "../../resources/icons/lock_open.png";
+import WithActionSheet from "../ui-elements/ActionSheet/WithActionSheet";
 
 const FrontLock = () => {
   const { hass } = useHass();
   const { states } = hass;
   const data = states["lock.front_lock_lock"];
 
-  // const lockDoor = () =>
-  //   hass.callService("lock", "lock", {
-  //     entity_id: "lock.front_lock_lock",
-  //   });
+  const lockDoor = () =>
+    hass.callService("lock", "lock", {
+      entity_id: "lock.front_lock_lock",
+    });
 
-  // const openDoor = () =>
-  //   hass.callService("lock", "open", {
-  //     entity_id: "lock.front_lock_lock",
-  //   });
+  const openDoor = () =>
+    hass.callService("lock", "open", {
+      entity_id: "lock.front_lock_lock",
+    });
 
   const isUnlocked = data.state === "unlocked";
   const isUnavailable = data.state === "unavailable";
 
   return (
-    <DashboardCard variant={isUnlocked && "warning"}>
-      <div className="front-lock flex-col-center">
-        {isUnavailable && <UnavailableBadge />}
-        <img src={isUnlocked ? lockOpen : lockClosed} />
-        <h3>Front Lock</h3>
-      </div>
-    </DashboardCard>
+    <WithActionSheet
+      options={{
+        title: "Front Door",
+        subtitle: `Front door is ${isUnlocked ? "unlocked" : "locked"}`,
+        actions: [
+          {
+            caption: isUnlocked ? "Lock" : "Unlock",
+            onClick: isUnlocked ? lockDoor : openDoor,
+            isDestructive: !isUnlocked,
+          },
+        ],
+      }}
+    >
+      <DashboardCard variant={isUnlocked && "warning"}>
+        <div className="front-lock flex-col-center">
+          {isUnavailable && <UnavailableBadge />}
+          <img src={isUnlocked ? lockOpen : lockClosed} />
+          <h3>Front Lock</h3>
+        </div>
+      </DashboardCard>
+    </WithActionSheet>
   );
 };
 
