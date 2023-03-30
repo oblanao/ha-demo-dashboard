1ac7c05ea8d30519d4efa8eeb221b79d555158bc
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-03-30 10:39:01 +0300

    switch to real hass

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { HassContext } from "../../context";
 import Dashboard from "../Dashboard";
-import mockHass from "../../data/examples/hass";
+// import mockHass from "../../data/examples/hass";
 
 const App = ({ hass }) => {
   return (
-    <HassContext.Provider value={mockHass}>
+    <HassContext.Provider value={hass}>
       {/* Rendering demo component */}
       <Dashboard />
     </HassContext.Provider>
